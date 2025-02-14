874e8518d98519cd91aa6f0d935304e1c8cf52d2
Mustafa <mustafaagulden@gmail.com>
2025-02-14 16:16:36 +0300

    Initial Commit

---
+++ EDisonTechCaseStudy/EDisonTechCaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EDisonTechCaseStudyApp.swift
+//  EDisonTechCaseStudy
+//
+//  Created by Mustafa on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct EDisonTechCaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
